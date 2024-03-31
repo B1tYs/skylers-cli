d6da05dde53a003813f5a82988fdf1bd4db58625
bouldersky <skyarnold1@me.com>
2024-03-30 21:52:38 -0600

    feat: Add command to pretty print dicom metadata

--- tests/cli/test_main.py
+++ tests/cli/test_main.py
@@ -1,14 +1,11 @@
-from typer.testing import CliRunner
 from skyler_cli.cli import main
-
-runner = CliRunner()
 
 
 class TestNoArgs:
-    def test_no_args_exit_code(self):
-        result = runner.invoke(main.app, [])
+    def test_no_args_exit_code(self, cli_runner):
+        result = cli_runner.invoke(main.app, [])
         assert result.exit_code == 0
 
-    def test_no_args_prints_help(self):
-        result = runner.invoke(main.app, [])
+    def test_no_args_prints_help(self, cli_runner):
+        result = cli_runner.invoke(main.app, [])
         assert main.help_msg in result.stdout
---
+++ tests/dicom/dicom_parse.py
@@ -0,0 +1,28 @@
+import json
+from pathlib import Path
+
+from skyler_cli.cli import main
+
+
+class TestParseSingleDICOMFile:
+    def test_parse_single_file_to_pprint(self, cli_runner):
+        test_file_path = Path(__file__).parent / "SC_rgb_jpeg.dcm"
+        result = cli_runner.invoke(main.app, ["dicom", str(test_file_path)])
+        print(result.stdout)
+        assert result.exit_code == 0
+        assert "File Meta Information Group Length" in result.stdout
+
+    def test_parse_single_file_to_json(self, cli_runner):
+        test_file_path = Path(__file__).parent / "SC_rgb_jpeg.dcm"
+        result = cli_runner.invoke(
+            main.app, ["dicom", str(test_file_path), "--fmt", "json"]
+        )
+        assert result.exit_code == 0
+
+        result_json = json.loads(result.stdout)
+        assert result_json["00080018"] == {
+            "Value": [
+                "1.2.826.0.1.3680043.8.498.13002811185086637637347356263722492924"
+            ],
+            "vr": "UI",
+        }
